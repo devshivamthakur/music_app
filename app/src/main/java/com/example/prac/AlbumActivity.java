648de5d07c0097e3e3ceebd6bb4bc1e35846c48a
shivam <shivamthakurcool01@gmail.com>
2020-10-15 18:18:26 +0530

    add content of album activity_album.xml

--- app/src/main/java/com/example/prac/AlbumActivity.java
+++ app/src/main/java/com/example/prac/AlbumActivity.java
@@ -1,14 +1,23 @@
 package com.example.prac;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AlbumActivity extends AppCompatActivity {
+    private TextView album_name;
+    private ImageView album_img;
+    private RecyclerView album_related_recycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album);
+        album_img = findViewById(R.id.album_activity_img);
+        album_name = findViewById(R.id.album_name);
+        album_related_recycler = findViewById(R.id.album_related_song_recycler);
     }
 }
